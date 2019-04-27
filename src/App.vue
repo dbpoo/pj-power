4134143916dbe1cc6fd96d555ee45b4b53702cda
wei <weiwei198328@gmail.com>
2019-04-27 09:19:09 +0800

    导航部门代码

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="nav-flex">
+        <div class="nav-logo">
+          <router-link to="/">logo</router-link>
+        </div>
+        <div class="nav-link">
+          <router-link to="/">解决方案</router-link>
+          <router-link to="/">交易平台</router-link>
+          <router-link to="/">研究院</router-link>
+          <router-link to="/">新闻动态</router-link>
+          <router-link to="/">关于我们</router-link>
+        </div>
+      </div>
     </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "css/reset";
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  height: 78px;
+  background: url("./assets/top.jpg") center 0 no-repeat;
+  .nav-flex {
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .nav-logo {
+      a {
+        width: 148px;
+        height: 77px;
+        display: block;
+        background: url("./assets/logo.png") 0 0 no-repeat;
+        text-indent: -9999px;
+      }
+    }
+    .nav-link {
+      a {
+        margin: 0 20px;
+        color: #fff;
+        &:hover{
+          color: #29acf1;
+        }
+        &:last-child {
+          margin-right: 0;
+        }
+      }
     }
   }
 }
